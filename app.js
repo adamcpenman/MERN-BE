84b5b1c73c9b490052bf92d820e1858b4af00012
Adam <adamcpenman@gmail.com>
2020-04-21 11:53:30 -0500

    added mongoose and connected to MongoDB server

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const placesRoutes = require('./routes/places-routes');
 const usersRoutes = require('./routes/users-routes');
 const HttpError = require('./models/http-error');
+
+require('dotenv').config();
 
 const app = express();
 
@@ -27,6 +30,16 @@ app.use((error, req, res, next) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () =>
-  console.log(`Server listening at http://localhost:${PORT}`)
-);
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    app.listen(PORT, () =>
+      console.log(`Server listening at http://localhost:${PORT}`)
+    );
+  })
+  .catch((err) => {
+    console.log(err);
+  });
