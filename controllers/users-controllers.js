24e50dbaa797e40f5d5f177c64310b4c5baf2053
Adam <adamcpenman@gmail.com>
2020-05-11 10:19:39 -0500

    hashing password when signing up

--- controllers/users-controllers.js
+++ controllers/users-controllers.js
@@ -1,4 +1,5 @@
 const { validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
 
 const HttpError = require('../models/http-error');
 const User = require('../models/user');
@@ -46,11 +47,19 @@ const signup = async (req, res, next) => {
     return next(error);
   }
 
+  let hashedPassword;
+  try {
+    hashPassword = await bcrypt.hash(password, 12);
+  } catch {
+    const error = new HttpError('Could not create user.', 500);
+    return next(error);
+  }
+
   const createdUser = new User({
     name,
     email,
     image: req.file.path,
-    password,
+    password: hashedPassword,
     places: [],
   });
 
