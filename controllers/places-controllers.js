5544e454501eb2555ec42c61a6e597e618aa5e8b
Adam <adamcpenman@gmail.com>
2020-05-11 13:50:44 -0500

    checking for ids

--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -124,6 +124,11 @@ const updatePlace = async (req, res, next) => {
     return next(error);
   }
 
+  if (place.creator.toString() !== req.userData.userId) {
+    const error = new HttpError('You are not allowed to edit', 401);
+    return next(error);
+  }
+
   place.title = title;
   place.description = description;
 
@@ -153,6 +158,11 @@ const deletePlace = async (req, res, next) => {
 
   if (!place) {
     const error = new HttpError('Could not find place for id', 404);
+    return next(error);
+  }
+
+  if (place.creator.id !== req.userData.userId) {
+    const error = new HttpError('You are not allowed to edit', 401);
     return next(error);
   }
 
